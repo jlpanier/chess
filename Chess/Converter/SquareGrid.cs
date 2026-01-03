1a2767b2e6b1e711ac9ec28240b1d1ce549a11e2
Jean-Luc <jean-luc.panier@outlook.com>
2026-01-03 22:35:19 +0100

    Affichage des pièces

---
+++ Chess/Converter/SquareGrid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Converter
+{
+    public class SquareGrid : Grid
+    {
+        protected override Size MeasureOverride(double widthConstraint, double heightConstraint)
+        {
+            double size = Math.Min(widthConstraint, heightConstraint);
+            return base.MeasureOverride(size, size);
+        }
+
+        protected override Size ArrangeOverride(Rect bounds)
+        {
+            double size = Math.Min(bounds.Width, bounds.Height);
+            var square = new Rect(bounds.X, bounds.Y, size, size);
+            return base.ArrangeOverride(square);
+        }
+    }
+}
