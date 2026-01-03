f728e409ac67a1505be941726b831ecee7f97e34
Jean-Luc <jean-luc.panier@outlook.com>
2026-01-03 23:06:50 +0100

    Initilisation de l'échiquier

--- Chess/Converter/SquareColorConverter.cs
+++ Chess/Converter/SquareColorConverter.cs
@@ -19,7 +19,14 @@ namespace Chess.Converter
 
                 bool isDark = (row + col) % 2 == 1;
 
-                return isDark ? Colors.SaddleBrown : Colors.Bisque;
+                //return isDark ? Color.FromArgb("#769656") : Color.FromArgb("#EEEED2");
+                //return isDark ? Color.FromArgb("#A67C00") : Color.FromArgb("#F2E9D0");
+                //return isDark ? Color.FromArgb("#5A6E7F") : Color.FromArgb("#C7D3DD");
+                //return isDark ? Color.FromArgb("#4A5568") : Color.FromArgb("#DCE1E3");
+                //return isDark ? Color.FromArgb("#4A752C") : Color.FromArgb("#F0F0F0");
+                //return isDark ? Color.FromArgb("#EEEED2") : Color.FromArgb("#769656");
+                //return isDark ? Color.FromArgb("#8B5A2B") : Color.FromArgb("#EEDFCC");
+                return isDark ? Color.FromArgb("#B58863") : Color.FromArgb("#F0D9B5");
             }
 
             return Colors.Transparent;
